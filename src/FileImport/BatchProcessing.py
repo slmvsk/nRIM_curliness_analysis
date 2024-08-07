98d0d2e65e8cbba08b5a447ee3df61dfba5b5f80
Tetiana S. <92734601+slmvsk@users.noreply.github.com>
2024-08-07 11:17:40 +0900

    Update BatchProcessing.py

--- src/FileImport/BatchProcessing.py
+++ src/FileImport/BatchProcessing.py
@@ -93,6 +93,7 @@ def batch_process_files(file_list, process_function, folder_path):
         for scene_index, cleaned_scene in enumerate(cleaned_scenes):
             all_rows.append({
                 "file_name": file_name,
+                "file_index": file_index
                 "scene_index": scene_index,
                 "cleaned_scene": cleaned_scene
             })
@@ -117,7 +118,21 @@ file_list = getMatchingFilesList(
 
 
 dataframe_results = batch_process_files(file_list, processFile, folder_with_data)
- 
+print(dataframe_results.head)
+dataframe_results.to_csv('/Users/tetianasalamovska/Desktop/zeis/df.csv', index=False)
+
+# Example: Get data for file_index 0, scene_index 2
+filtered_df = df[(df['file_index'] == 0) & (df['scene_index'] == 2)]
+
+# Example: Get all slices for a specific filename
+specific_file_df = df[df['filename'] == 'example_filename.czi']
+
+plot_images(dataframe_results[(dataframe_results['file_name'] == 'IHCT_THT53_40x2x_IHCT08_slice6_stack_positions_A488_laser08_speed6.czi') & (dataframe_results['scene_index'] == 6)], dataframe_results[(dataframe_results['file_name'] == 'IHCT_THT53_40x3x_IHCT08_slice6_stack_positions_A488_laser08_speed6.czi') & (dataframe_results['scene_index'] == 6)], 'Original', 'No Somata')
+
+
+
+
+
 
 
 
@@ -125,12 +140,67 @@ def release_memory(variable):
     del variable
 
 
+import matplotlib.pyplot as plt
+
+def plotImages(image1, image2, title1, title2):
+    """
+    Plot two images side by side for comparison.
+
+    Args:
+    image1, image2 (numpy.ndarray): The images to plot.
+    title1, title2 (str): Titles for the two images.
+    """
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(image1, cmap='gray')
+    axes[0].set_title(title1)
+    axes[0].axis('off')  # Turn off axis numbering and ticks
+
+    axes[1].imshow(image2, cmap='gray')
+    axes[1].set_title(title2)
+    axes[1].axis('off')
+
+    plt.show()
 
 
 
-
-
+# Print summary of the DataFrame to check its content
+print(dataframe_results.head())
+print(dataframe_results['file_name'].unique())  # Check the unique filenames to ensure your file is listed
+print(dataframe_results['scene_index'].unique())  # Check the unique scene indices
+def fetch_and_plot_images(df, file_name_1, scene_index_1, file_name_2, scene_index_2):
+    """
+    Fetch two images based on file name and scene index, and plot them side by side.
     
+    Args:
+    df (DataFrame): The DataFrame containing the images and metadata.
+    file_name_1 (str): File name of the first image.
+    scene_index_1 (int): Scene index of the first image.
+    file_name_2 (str): File name of the second image.
+    scene_index_2 (int): Scene index of the second image.
+    """
+    image1_data = df.loc[(df['file_name'] == file_name_1) & (df['scene_index'] == scene_index_1), 'cleaned_scene'].iloc[0]
+    image2_data = df.loc[(df['file_name'] == file_name_2) & (df['scene_index'] == scene_index_2), 'cleaned_scene'].iloc[0]
     
+    if image1_data is not None and image2_data is not None:
+        plot_images(image1_data, image2_data, 'Image 1', 'Image 2')
+    else:
+        print("One or both of the images could not be found.")
+
+# Usage example
+fetch_and_plot_images(dataframe_results, 
+                      '/Users/tetianasalamovska/Desktop/zeis/IHCT_THT53_40x2x_IHCT08_slice6_stack_positions_A488_laser08_speed6.czi', 0, 
+                      '/Users/tetianasalamovska/Desktop/zeis/IHCT_THT53_40x2x_IHCT08_slice6_stack_positions_A488_laser08_speed6.czi', 1)
+
+
+# validation and problems in the tg notes 
+# check and discuss 
+# but overall data is saved normally to the dataframe and is normally executed when 
+# needed like above an example with plotting 
+# but I maybe need top add file index as well because it can be inconvinient 
+# to write whole file name (but you will know for sure with which file are you working with
+# --- > decide on the next stage of analysis)
+
+
+
 
 
